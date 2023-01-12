f356e2e44dba5988362ecab728439e644201b3d7
Antonio Alves <antoniocunha9610@gmail.com>
2023-01-12 12:02:25 -0300

    Position and board game

---
+++ aplication/Programs.java
@@ -0,0 +1,11 @@
+import boardGame.Position;
+
+public class Programs {
+    public static void main(String[] args) {
+        System.out.println("Jogo de xadez");
+
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+    }
+}
